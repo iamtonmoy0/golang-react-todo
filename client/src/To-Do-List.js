6b966cb1540765f10ce7ec7aa9574e7681dfa52e
iamtonmoy0 <tonmoy2code@gmail.com>
2023-02-08 11:15:32 -0500

    server partition added

--- client/src/To-Do-List.js
+++ client/src/To-Do-List.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import {Card,Header,Form,Input,Icon} from "semantic-ui-react";
 
 let endpoint = "http://localhost:9000";
+
+
 class ToDoList extends Component{
 	constructor(props){
 		super(props);
@@ -12,17 +14,25 @@ class ToDoList extends Component{
 		};
 	}
 
-ComponentDidMount(){
+       ComponentDidMount(){
 	this,getTask();
-}
+          }
 
 render(){
 	return(
 		<div>
-			<div className="row"></div>
+			<div className="row">
 			<Header className="header" as="h2" color="yellow">
-
+TO DO LIST
 			</Header>
+			</div>
+			<div className="row">
+				<form onSubmit={this .onSubmit}>
+					<input type="text" name="task" onChange={this.onChange} value={this.state.task
+					} fluid placeholder="Create Task"></input>
+
+				</form>
+			</div>
 		</div>
 	)
 }
