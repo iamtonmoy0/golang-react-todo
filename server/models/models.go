3b7fdf1639dfc99c58fdd9591e9f55adb0c38dda
iamtonmoy0 <tonmoy2code@gmail.com>
2023-02-08 11:25:30 -0500

    model struct added

--- server/models/models.go
+++ server/models/models.go
@@ -1,1 +1,9 @@
 package models
+
+import "go.mongodb.org/monto-driver/bson/primitive"
+
+type ToDoList struct {
+	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Task   string             `json:"task,omitempty"`
+	Status bool               `json:"status,omitempty"`
+}
