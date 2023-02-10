9953654859e4d7577cba47a9c35db6d9ddedd745
iamtonmoy0 <tonmoy2code@gmail.com>
2023-02-10 13:52:11 -0500

    typw error fixed

--- server/models/models.go
+++ server/models/models.go
@@ -1,6 +1,6 @@
 package models
 
-import "go.mongodb.org/monto-driver/bson/primitive"
+import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type ToDoList struct {
 	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
